fac91a52f10f50e1986d4a35b413f1e555c62f9a
bieniucieniu <mikibien@outlook.com>
2023-01-06 04:00:28 +0100

    change bg  and move types to devDep

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Game } from "./game/Game";
 
 export const App = () => {
   return (
-    <div className="w-screen h-screen grid bg-zinc-900">
+    <div className="w-screen h-screen grid bg-zinc-600">
       <nav className="h-12 bg-cyan-300"> </nav>
       <Game />
     </div>
