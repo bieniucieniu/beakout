0635c5b2abe24df63a483bfd297b557d79ffd7ce
bieniucieniu <mikibien@outlook.com>
2023-01-19 23:07:33 +0100

    controls form pad

--- src/game/Pad.tsx
+++ src/game/Pad.tsx
@@ -1,14 +1,39 @@
-type PadProps = {
-  position: [number, number, number];
-  size: [number, number, number];
-  color?: string;
-};
+import { useBox } from "@react-three/p2";
 
-export const Pad = ({ position, size, color }: PadProps) => {
+import type { PadProps } from "../types";
+
+export const Pad = ({ position, color, size, material }: PadProps) => {
+  const [ref, api] = useBox(() => ({
+    type: "Kinematic",
+    position: [position[0], position[1]],
+    args: size,
+    collisionResponse: true,
+    material,
+  }));
+
+  window.addEventListener("keydown", (e) => {
+    if (e.key === "ArrowLeft" || e.key === "a") {
+      api.velocity.set(-7, 0);
+    } else if (e.key === "ArrowRight" || e.key === "d") {
+      api.velocity.set(7, 0);
+    }
+  });
+
+  window.addEventListener("keyup", (e) => {
+    if (e.key === "ArrowLeft" || e.key === "a") {
+      api.velocity.set(0, 0);
+    } else if (e.key === "ArrowRight" || e.key === "d") {
+      api.velocity.set(0, 0);
+    }
+  });
+
   return (
-    <mesh position={position}>
-      <boxGeometry args={size} />
-      <meshStandardMaterial color={color || "red"} />
-    </mesh>
+    <>
+      {/* @ts-ignore */}
+      <mesh ref={ref} name={"pad"}>
+        <boxGeometry args={[...size, 1]} />
+        <meshStandardMaterial color={color} />
+      </mesh>
+    </>
   );
 };
