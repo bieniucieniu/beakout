f85991d65a8790e8fd4c31884be26ce8e442aa10
bieniucieniu <mikibien@outlook.com>
2023-01-19 23:08:38 +0100

    bricks pos setup , materials,

--- src/game/Board.tsx
+++ src/game/Board.tsx
@@ -1,35 +1,84 @@
-import type { BrickProps } from "./Brick";
 import { Brick } from "./Brick";
 import { Ball } from "./Ball";
+import type { BrickProps, BoardProps } from "../types";
+import { Physics, useContactMaterial } from "@react-three/p2";
+import { Border } from "./Border";
+import { useRef, useState } from "react";
+import { Pad } from "./Pad";
+import { BricksGrid } from "./BricksGrid";
 
 const BOARD_WIDTH = 32;
 const BOARD_HEIGHT = 24;
 const GRID_WIDTH = 12;
 const GRID_HEIGHT = 6;
 
-type BoardProps = {
-  size: [number, number];
-  position?: [number, number, number];
-  bricks: BrickProps[];
-  setBricks: (bricks: BrickProps[]) => void;
-};
+export const Board = ({ size }: BoardProps) => {
+  const bricksRef = useRef<BrickProps[]>(
+    Array.from({ length: GRID_WIDTH * GRID_HEIGHT }, (_, i) => ({
+      position: [
+        (i % GRID_WIDTH) * (BOARD_WIDTH / GRID_WIDTH) -
+          BOARD_WIDTH / 2 +
+          BOARD_WIDTH / GRID_WIDTH / 2,
+        Math.floor(i / GRID_WIDTH) * (BOARD_HEIGHT / GRID_HEIGHT / 2) +
+          BOARD_HEIGHT / GRID_HEIGHT / 2 -
+          1,
+        0,
+      ],
+      size: [2, 1],
+      depth: 1,
+      color: "red",
+      points: 1,
+      name: `brick-${i}`,
+    }))
+  );
 
-export const Board = ({ size, position, bricks, setBricks }: BoardProps) => {
+  const PadMaterial = {
+    id: 1,
+    restitution: 1,
+    friction: 0,
+  };
+
+  const BallMaterial = {
+    id: 2,
+    restitution: 1,
+    friction: 0,
+  };
+
+  const defaultMaterial = {
+    id: 3,
+    restitution: 0.9,
+    friction: 0,
+  };
+
+  useContactMaterial(PadMaterial, BallMaterial, {
+    friction: 0,
+    restitution: 1.2,
+  });
+
   return (
     <>
-      <mesh position={position}>
+      <mesh position={[0, 0, -1]}>
         <planeGeometry args={size} />
         <meshStandardMaterial color="hotpink" />
-        {bricks?.map(
-          (brick) =>
-            brick.points && <Brick key={brick.position.join()} {...brick} />
-        )}
-        <Ball
-          boardSize={[BOARD_WIDTH, BOARD_HEIGHT]}
-          bricks={bricks}
-          setBricks={setBricks}
-        />
       </mesh>
+
+      <BricksGrid bricksRef={bricksRef} material={defaultMaterial} />
+
+      <Ball name="ball" material={BallMaterial} />
+
+      <Border
+        boardSize={[BOARD_WIDTH, BOARD_HEIGHT]}
+        height={1}
+        depth={2}
+        color="red"
+        material={defaultMaterial}
+      />
+      <Pad
+        position={[0, -BOARD_HEIGHT / 2 + 1]}
+        size={[5, 1]}
+        color="red"
+        material={PadMaterial}
+      />
     </>
   );
 };
