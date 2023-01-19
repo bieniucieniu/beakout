cacf0b998edb585ce4415dd7d79fbb017491db9f
bieniucieniu <mikibien@outlook.com>
2023-01-19 21:38:05 +0100

    game.tsx -> index.tsx moved physics to it

---
+++ src/game/index.tsx
@@ -0,0 +1,29 @@
+import { Canvas } from "@react-three/fiber";
+import { Lights } from "./Lights";
+import { OrbitControls } from "@react-three/drei";
+import { Board } from "./Board";
+import type { GameProps } from "../types";
+import { Physics } from "@react-three/p2";
+const BOARD_WIDTH = 32;
+const BOARD_HEIGHT = 24;
+const GRID_WIDTH = 12;
+const GRID_HEIGHT = 6;
+
+export default ({ className }: GameProps) => {
+  return (
+    <Canvas className={className} camera={{ position: [0, 0, 18] }}>
+      <OrbitControls />
+      <Physics
+        normalIndex={2}
+        gravity={[0, -6]}
+        defaultContactMaterial={{
+          friction: 0,
+          restitution: 1,
+        }}
+      >
+        <Board size={[BOARD_WIDTH, BOARD_HEIGHT]} />
+      </Physics>
+      <Lights boardSize={[32, 24]} />
+    </Canvas>
+  );
+};
