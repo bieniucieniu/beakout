687130a72f4f2923df52bebf955eee17bb69c7ff
bieniucieniu <mikibien@outlook.com>
2023-01-24 10:37:43 +0100

    on/off button for game

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
+import { useState } from "react";
 import Game from "./game";
 
 export const App = () => {
+  const [isPlaying, setIsPlaying] = useState(false);
+
   return (
     <div className="w-screen h-screen grid bg-zinc-600">
-      <nav className="h-12 bg-cyan-300"> </nav>
-      <Game />
+      <nav className="h-12 bg-cyan-300">
+        <button className="" onClick={() => setIsPlaying(!isPlaying)}>
+          play
+        </button>
+      </nav>
+      {isPlaying && <Game />}
     </div>
   );
 };
