8db6d06fb4b56390f42d5bd538d313119acc4818
bieniucieniu <mikibien@outlook.com>
2023-01-30 22:26:39 +0100

    awaikening trigerd only by vertical vel

--- src/game/Ball.tsx
+++ src/game/Ball.tsx
@@ -42,11 +42,12 @@ export const Ball = ({ material, name, boardSize, margin }: BallProps) => {
     } else if (posRef.current[1] >= boardSize[1] / 2 + margin) {
       api.position.set(posRef.current[0], boardSize[1] / 2 - margin);
     }
+    console.log(velocityRef.current);
 
     if (
-      velocityRef.current[0] < 0.0001 &&
+      // velocityRef.current[0] < 0.0001 &&
       velocityRef.current[1] < 0.0001 &&
-      velocityRef.current[0] > -0.0001 &&
+      // velocityRef.current[0] > -0.0001 &&
       velocityRef.current[1] > -0.0001
     ) {
       VelTimerRef.current++;
